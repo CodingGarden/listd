df5a086cc36569ac1059b2e40d30e48ffc8813eb
w3cj <cj@null.computer>
2023-02-10 14:19:18 -0700

    :sparkles: :package: update dependencies

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -10,18 +10,18 @@
 
 <p>{$LL.onboarding.messages.main()}</p>
 <form class="pt-4" method="POST" use:enhance={() => async () => {}}>
-	<label for="name">
+	<label for="name" class="label">
 		<span>{$LL.onboarding.labels.username()}</span>
-		<input type="text" id="username" value="w3cj" minlength="4" required />
+		<input class="input" type="text" id="username" value="w3cj" minlength="4" required />
 	</label>
 	<p class="my-4">{$LL.onboarding.messages.avatar()}</p>
 	<div class="flex justify-center">
-		<FileButton bind:files button="btn-filled-primary"
+		<FileButton name="avatar" bind:files button="variant-filled-primary"
 			>{$LL.onboarding.labels.uploadFile()}</FileButton
 		>
 	</div>
 	<p class="pt-4">{$LL.onboarding.messages.final()}</p>
 	<div class="my-4 flex justify-end">
-		<button class="btn btn-base btn-filled-success">Lets Go!</button>
+		<button class="btn variant-filled-success">Lets Go!</button>
 	</div>
 </form>
