6d4bcbf791da6072f5f8b95515bedcf5f9f5512f
w3cj <cj@null.computer>
2023-03-03 13:03:41 -0700

    :sparkles: add auth loading states fix: #71

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 	// import { IconBrandYoutube } from '@tabler/icons-svelte';
 	import { LL } from '$lib/i18n/i18n-svelte';
 	import { page } from '$app/stores';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
+	let loading = false;
 </script>
 
 <div class="hero-container flex flex-col items-center justify-center p-4">
@@ -11,8 +14,19 @@
 	{#if $page.data.session}
 		<p>{$page.data.session.user?.name ?? 'User'} logged in</p>
 	{:else}
-		<button on:click={() => signIn('google')} class="btn variant-filled-primary cursor-pointer">
-			{$LL.signUp()}
+		<button
+			on:click|once={function loginClick() {
+				this.disabled = true;
+				loading = true;
+				signIn('google');
+			}}
+			class="btn variant-filled-primary cursor-pointer"
+		>
+			{#if loading}
+				{$LL.pleaseWait()} <ProgressRadial class="ml-2 h-6 w-6" stroke={100} />
+			{:else}
+				{$LL.loginYouTube()}
+			{/if}
 			<!-- <IconBrandYoutube class="ml-1" size={36} stroke={1} /> -->
 		</button>
 	{/if}
