66c81d7fb3b8bf36363e757aa2e135104a0ec1b2
Dipam Sen <dipamdiptam@gmail.com>
2023-03-09 14:25:56 +0530

    extract AuthButton component

--- src/routes/NavTrail.svelte
+++ src/routes/NavTrail.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-	import { LightSwitch, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import { signOut } from '@auth/sveltekit/client';
 	import { LL } from '$lib/i18n/i18n-svelte';
 	import { page } from '$app/stores';
-
-	let loading = false;
-
-	const signOutClick = (event: { currentTarget: EventTarget & HTMLButtonElement }) => {
-		event.currentTarget.disabled = true;
-		loading = true;
-		signOut();
-	};
+	import AuthButton from '$/lib/components/AuthButton.svelte';
 </script>
 
 <!-- TODO: remove after next skeleton update -->
@@ -25,11 +18,5 @@
 		/>
 		<p class="hidden font-bold md:block">{$page.data.session?.user?.name}</p>
 	</div>
-	<button on:click|once={signOutClick} class="btn variant-filled-primary">
-		{#if loading}
-			{$LL.pleaseWait()} <ProgressRadial class="ml-2 h-6 w-6" stroke={100} />
-		{:else}
-			{$LL.logOut()}
-		{/if}
-	</button>
+	<AuthButton clickHandler={() => signOut()} content={$LL.logOut()} />
 {/if}
