54c2d033d1729c69bb75f398e37a13b428f568ea
Dipam Sen <dipamdiptam@gmail.com>
2023-03-09 14:32:52 +0530

    rename function

--- src/lib/components/AuthButton.svelte
+++ src/lib/components/AuthButton.svelte
@@ -7,14 +7,14 @@
 	export let content: string;
 	export let clickHandler: () => void;
 
-	const load = (event: { currentTarget: EventTarget & HTMLButtonElement }) => {
+	const handle = (event: { currentTarget: EventTarget & HTMLButtonElement }) => {
 		loading = true;
 		event.currentTarget.disabled = true;
 		clickHandler();
 	};
 </script>
 
-<button on:click={load} class="btn variant-filled-primary">
+<button on:click={handle} class="btn variant-filled-primary">
 	{#if loading}
 		{$LL.pleaseWait()} <ProgressRadial class="ml-2 h-6 w-6" stroke={100} />
 	{:else}
