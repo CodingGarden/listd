8b6b2ef9d1f0d1be255795db920075e1beec0097
vishalbalaji <vishalbalaji@xyz>
2023-03-24 21:56:54 +0530

    :rocket: stream video data to page

--- src/routes/list/[id]/+page.svelte
+++ src/routes/list/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,21 +11,27 @@
 	<p>{item.name}</p>
 {/each} -->
 <div>
-	{#each data.videos as video}
-		<div class="card">
-			<!-- <img
-				class="w-full"
-				src={video.snippet?.thumbnails?.maxres?.url || video.snippet?.thumbnails?.default?.url}
-				alt={video.snippet?.title} /> -->
-			<iframe
-				class="w-full"
-				src={`https://www.youtube.com/embed/${video.id?.videoId}`}
-				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowfullscreen />
-			<p class="text-center font-bold">{video.snippet?.channelTitle}</p>
-			<p class="text-center">{video.snippet?.title}</p>
-		</div>
-	{/each}
+	{#await data.streamed.videos}
+		<span class="grid w-full place-items-center p-4">
+			<ProgressRadial class="ml-2 h-8 w-8" stroke={100} />
+		</span>
+	{:then videos}
+		{#each videos as video}
+			<div class="card">
+				<!-- <img
+					class="w-full"
+					src={video.snippet?.thumbnails?.maxres?.url || video.snippet?.thumbnails?.default?.url}
+					alt={video.snippet?.title} /> -->
+				<iframe
+					class="w-full"
+					src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen />
+				<p class="text-center font-bold">{video.snippet?.channelTitle}</p>
+				<p class="text-center">{video.snippet?.title}</p>
+			</div>
+		{/each}
+	{/await}
 </div>
